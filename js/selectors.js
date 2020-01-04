243cea1260a12a10a2282df038c6fd4a58fefbcf
Omar Gonzalez <ogonzaleznyc@gmail.com>
2020-01-04 03:28:22 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,16 +1,36 @@
 // declare your functions here...
-function paragraphSelector() { return $('p');
+// function paragraphSelector() { return $('p');
+// }
+
+// function lastImageSelector() { return $('img:last');
+// }
+
+// function ninjaBabySelector() { return $('#baby-ninja');
+// }
+
+// function divSelector() { return $('.pics');
+// }
+
+// function firstListItem() { return $('#pic-list li:first-child');
+
+// }
+
+function paragraphSelector() {
+  return $('p');
 }
 
-function lastImageSelector() { return $('img:last');
+function lastImageSelector() {
+  return $('img:last');
 }
 
-function ninjaBabySelector() { return $('#baby-ninja');
+function ninjaBabySelector() {
+  return $('#baby-ninja')
 }
 
-function divSelector() { return $('.pics');
+function divSelector() {
+  return $('.pics')
 }
 
-function firstListItem() { return $('#pic-list li:first-child');
-
+function firstListItem() {
+  return $('ul #pic-list:first-child')
 }
